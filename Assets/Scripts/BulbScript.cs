a97a81b1c8f6c5f987f96ee76348d3a2e95ede57
LIT_Ryan <77435288+RyanLim1115@users.noreply.github.com>
2021-01-19 15:56:04 +0000

    change1
    1/18/2021

---
+++ Assets/Scripts/BulbScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbScript : MonoBehaviour
+{
+    private Animator BulbAnimator;
+    private bool BulbBool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BulbAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulbOn();
+        BulbOff();
+    }
+
+    void BulbOn()
+    {
+
+    }
+
+    void BulbOff()
+    {
+
+    }
+
+}
